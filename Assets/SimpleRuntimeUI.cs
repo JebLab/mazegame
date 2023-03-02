42e025ad08903da72252ac1b53b84e6277b068fa
RealProgrammersDontUseJavaScript <awburrell@mc.edu>
2023-03-02 08:57:22 -0600

    Fixed button scripts & added scene transition

--- Assets/SimpleRuntimeUI.cs
+++ Assets/SimpleRuntimeUI.cs
@@ -1,34 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.SceneManagement;
-
 
 public class SimpleRuntimeUI : MonoBehaviour
 {
     private Button _button1;
     private Button _button2;
-    private Button _Settings;
+
+    private int _clickCount;
 
 
     //Add logic that interacts with the UI controls in the `OnEnable` methods
     private void OnEnable()
     {
-        Debug.Log("<color=red>Youve entered onEnable()\n</color>");
         // The UXML is already instantiated by the UIDocument component
         var uiDocument = GetComponent<UIDocument>();
    
         _button1 = uiDocument.rootVisualElement.Q("Play") as Button;
-
-        Debug.Log("shawn");
-
         _button2 = uiDocument.rootVisualElement.Q("Quit") as Button;
-        _Settings = uiDocument.rootVisualElement.Q("Set") as Button;
 
         _button1.RegisterCallback<ClickEvent>(PrintClickMessage);
         _button2.RegisterCallback<ClickEvent>(PrintClickMessage);
-        _Settings.RegisterCallback<ClickEvent>(PrintClickMessage);
 
         var _inputFields = uiDocument.rootVisualElement.Q("input-message");
         _inputFields.RegisterCallback<ChangeEvent<string>>(InputMessage);
@@ -43,10 +34,10 @@ public class SimpleRuntimeUI : MonoBehaviour
 
     private void PrintClickMessage(ClickEvent evt)
     {
+        ++_clickCount;
 
-        /* Debug.Log($"{"Play"} was clicked, you handsome devil!");
-        Debug.Log($"{"Quit"} was clicked!");
-        Debug.Log($"{"Set"} was clicked!"); */
+        Debug.Log($"{"Play"} was clicked!");
+        // Debug.Log($"{"Quit"} was clicked!");
     }
 
     public static void InputMessage(ChangeEvent<string> evt)
